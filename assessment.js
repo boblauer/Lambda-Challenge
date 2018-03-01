5e0e39aa7b9ed12ab867b0dd35bca907d1abb96e
boblauer <rlauer@gmail.com>
2018-02-28 18:11:53 -0800

    Complete assessment

--- assessment.js
+++ assessment.js
@@ -1,75 +1,35 @@
-/*
-Work through the problems in this file.  As you work through each problem periodically run: npm test
-Your function name and the string must match the instructions exactly otherwise the tests will fail.
-After writing your function uncomment the matching function reference at the bottom of the file.
-*/
-
-// 1. Write a function called helloWorld that returns the string 'Hello World!'.
-
 function helloWorld() {
-
+  return 'Hello World!';
 }
 
-/*
-2. Write a function called lambdaSchool that has a single parameter called num.
-     num will be a positive integer.
-     If num is divisible by 3 return the string 'Lambda'
-     If num is divisible by 5 return the string 'School'
-     If num is divisible by 3 AND 5 return the string 'Lambda School' (notice the space)
-     If num is NOT divisible by 3 or 5 then return num.
-
-     Example:
-             lambdaSchool(15); // returns 'Lambda School'
-             lambdaSchool(8); // returns 8
-*/
-
-function lambdaSchool() {
-
+function lambdaSchool(num) {
+  if (num % 3 === 0 && num % 5 === 0) return 'Lambda School';
+  if (num % 3 === 0) return 'Lambda';
+  if (num % 5 === 0) return 'School';
+  return num;
 }
 
-/*
-3. Write a function called longestString that has a single parameter called strs.
-     strs will be an array of strings.
-     Return the longest string that is in strs.
-     If there is a tie for the longest string then return the one that occurs first.
+function longestString(strs) {
+  return strs.reduce((longestSoFar, s) => {
+    if (s.length > longestSoFar.length) {
+      return s;
+    }
 
-     Example:
-             longestString(['hi', 'hello', 'ni hao', 'guten tag']); // returns 'guten tag'
-             longestString(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
-*/
-
-function longestString() {
-
+    return longestSoFar;
+  }, '');
 }
 
-/*
-4. Write a function called computeUserAverageAge that has a single parameter called users
-     users is an array of user objects.
-     Each user object has a property called age that is a number.
-     Compute the average age of all user objects in the users array.
-     Round off the decimals if needed and return the number.
+function computeUserAverageAge(users) {
+  const agesSummed = users.reduce((runningTotal, user) => {
+    return runningTotal + user.age;
+  }, 0);
 
-     Example:
-             const users = [{
-               name: 'Brendan Eich',
-               age: 56,
-             }, {
-               name: 'Linus Torvalds',
-               age: 48,
-             }, {
-               name: 'Margaret Hamilton',
-               age: 81,
-             }];
-             computeUserAverageAge(users); // returns 62 (This number is rounded up from 61.6666)
-*/
-
-function computeUserAverageAge() {
-
+  return Math.round(agesSummed / users.length);
 }
 
 module.exports = {
- helloWorld,
- lambdaSchool,
- longestString,
- computeUserAverageAge
+  helloWorld,
+  lambdaSchool,
+  longestString,
+  computeUserAverageAge,
 };
